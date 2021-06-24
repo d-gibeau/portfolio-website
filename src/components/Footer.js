51a6cbb5f7b7247a8fa22d9b09927fe5ff90e152
Daniel Gibeau <dgibeau@gmail.com>
2021-06-23 20:08:55 -0600

    Basic layout

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>This Be Where Footer Go</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/NavMenu.js
@@ -0,0 +1,9 @@
+const NavMenu = () => {
+  return (
+    <nav>
+      <p>This Be Where Menu Go</p>
+    </nav>
+  );
+};
+
+export default NavMenu;
