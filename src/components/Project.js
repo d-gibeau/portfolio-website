0ef33fff5f61765a29ce93db52fb2dfcdf54db6e
Daniel Gibeau <dgibeau@gmail.com>
2021-07-02 17:16:00 -0600

    Draft of website added to project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 const Project = ({ imgSrc, imgAlt, projDesc, invert }) => {
   return (
-    <div className={invert ? `row project m-2 invert` : "row project m-2"}>
+    <div className={invert ? `row project invert` : "row project"}>
       <div className="col-lg-3 d-flex justify-content-center">
         <img src={imgSrc} className={`${imgAlt}-img proj-img`} alt={imgAlt} />
       </div>
