d830fc1f2b2c96ae437ad0ffadb9ebd91062ad35
Daniel Gibeau <dgibeau@gmail.com>
2022-08-26 02:04:25 -0600

    Let's try removing it and see if it builds

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,44 @@ import Home from "./components/pages/Home";
 import Feedback from "./components/pages/Feedback";
 import CurriculumVitae from "./components/pages/CurriculumVitae";
 import Projects from "./components/pages/Projects";
-import { createTheme, MuiThemeProvider } from "@material-ui/core/styles";
+// import { createTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import "./style/css/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#264653",
-      dark: "#1d353f",
-    },
-    secondary: {
-      main: "#b0bec5",
-      light: "#e2f1f8",
-      dark: "#808e95",
-    },
-  },
-  socialIcon: {
-    "&:hover": {
-      backgroundColor: "#e2f1f8 !important",
-    },
-  },
-});
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#264653",
+//       dark: "#1d353f",
+//     },
+//     secondary: {
+//       main: "#b0bec5",
+//       light: "#e2f1f8",
+//       dark: "#808e95",
+//     },
+//   },
+//   socialIcon: {
+//     "&:hover": {
+//       backgroundColor: "#e2f1f8 !important",
+//     },
+//   },
+// });
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Router>
-        <div className="wrapper">
-          <NavMenu />
-          <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/cv" component={CurriculumVitae} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Feedback} />
-        </div>
-        <Footer />
-      </Router>
-    </MuiThemeProvider>
+    // <MuiThemeProvider theme={theme}>
+    <Router>
+      <div className="wrapper">
+        <NavMenu />
+        <Route path="/" exact component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/cv" component={CurriculumVitae} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Feedback} />
+      </div>
+      <Footer />
+    </Router>
+    // </MuiThemeProvider>
   );
 }
 
